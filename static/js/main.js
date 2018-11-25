7f9c70f79d08a4cf54ac3854bab7ed55d2143233
Rhytah <ritanamono@gmail.com>
2018-11-25 11:49:18 +0300

    [Finishes  #162161621] include delete option to remove record feilds

--- static/js/main.js
+++ static/js/main.js
@@ -52,18 +52,3 @@ function opentab(evt, tabName ){
   evt.currentTarget.className += "active";
 
 }
-
-//acordion for 'my records'
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
