27ad45fd8d3b92fc567bde32894180a5a8ac914a
Rhytah <ritanamono@gmail.com>
2018-11-25 10:11:25 +0300

    [Finishes #162161611] user option to edit an existent record

--- static/js/main.js
+++ static/js/main.js
@@ -53,3 +53,17 @@ function opentab(evt, tabName ){
 
 }
 
+//acordion for 'my records'
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
