950d16affedb9575c9806a0debcebb1bcd168602
Rhytah <ritanamono@gmail.com>
2018-11-28 11:34:21 +0300

    refactor tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -6,13 +6,13 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.incident=dict(
-            id=1,
+            
             createdOn = "10-12-2014",
             createdBy = "sankyu",
             type = "red-flag",
             location = '123.01.56.78',
             status = "draft",
-            Images = ["Image","Image"],
-            videos = ["Image","Image"],
+            image = ["Image","Image"],
+            video = ["Image","Image"],
             comment = "Policeman asked for something something"
             )
