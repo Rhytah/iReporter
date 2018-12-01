3340674c650342ad110e6db4ec053a1093b7dc98
Rhytah <ritanamono@gmail.com>
2018-12-01 14:06:24 +0300

    validate spaces

--- reporter_api/utilities/incident_validators.py
+++ reporter_api/utilities/incident_validators.py
@@ -4,20 +4,20 @@ class Validation:
 
     
     def validate_incident(self,location,image,video,comment, created_by):
-        if not location or location == " ":
+        if not location or location.isspace():
             return jsonify({
                 "status":400,
                 "message":"location is missing"})
-        if not image or image == " ":
+        if not image or image.isspace():
             return jsonify({
                 "status":400,
                 "message":"image is missing"})
             
-        if not video or video == " ":
+        if not video or video.isspace():
             return jsonify({
                 "status":400,
                 "message":"video is missing"})
-        if not comment:
+        if not comment or comment.isspace():
             return jsonify({
                 "status":400,
                 "message":"comment is missing"})
@@ -26,15 +26,16 @@ class Validation:
             return jsonify({
                 "status":400,
                 "message":"created_by is missing"})
+        
 
     def validate_location(self,location):
-        if not location or location == " ":
+        if not location or location.isspace():
             return jsonify({
                 "status":400,
                 "message":"location is missing"})
 
     def validate_comment(self,comment):
-        if not comment:
+        if not comment or comment.isspace():
             return jsonify({
                 "status":400,
                 "message":"comment is missing"})
