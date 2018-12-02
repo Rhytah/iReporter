fc0b49f12687eeb8897c5617d4848e5b09dc940f
Rhytah <ritanamono@gmail.com>
2018-12-03 02:12:39 +0300

    include user views to app root

--- reporter_api/__init__.py
+++ reporter_api/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import app_configuration
+from flask_jwt_extended import JWTManager,jwt_required,create_access_token, get_jwt_identity
+
 
 app=Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'super-secret'  
+jwt = JWTManager(app)
 
 
 from reporter_api.views import incident_views, user_views
