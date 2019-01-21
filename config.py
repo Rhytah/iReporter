6b24e332ffca1bd7b08cc97ccacd7d3591568729
Rhytah <ritanamono@gmail.com>
2019-01-21 16:36:23 +0300

    declare database to connect to in configuration file

--- config.py
+++ config.py
@@ -1,14 +1,36 @@
-class Config:
-    DEBUG=False
+import os
 
+class Config:
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    JWT_ACCESS_TOKEN_EXPIRES = False
+    JWT_TOKEN_LOCATION = ['headers']
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
+    DATABASE_URI = os.getenv('DATABASE_URL')
+    
 
 class DevelopmentConfig(Config):
     DEBUG=True
+    ENV = 'development'
+    DATABASE = 'report_db'
+    TESTING = False
+
 
 class TestingConfig(Config):
     DEBUG=True
+    ENV = 'testing'
+    DATABASE = 'reporttest_db'
+    TESTING = True
+
+class ProductionConfig(Config):
+    DEBUG=False
+    
 
 app_configuration = {
     "development":DevelopmentConfig,
-    "testing":TestingConfig
+    "testing":TestingConfig,
+    "production":ProductionConfig
 }
+
