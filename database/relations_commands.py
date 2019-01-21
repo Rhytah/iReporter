b3e55af6495bca41424bede3405256c36b65fc75
Rhytah <ritanamono@gmail.com>
2019-01-21 16:51:02 +0300

    [Finishes #163362512] set up database server and relation commands

---
+++ database/relations_commands.py
@@ -0,0 +1,46 @@
+sqlcommands = (    
+                              
+                """
+                CREATE TABLE IF NOT EXISTS users(
+                    userid SERIAL PRIMARY KEY,
+                    firstname VARCHAR (30),
+                    lastname VARCHAR (30),
+                    username VARCHAR (30),
+                    password VARCHAR (10),
+                    email VARCHAR (30),
+                    phone_number INT,
+                    isadmin BOOLEAN DEFAULT FALSE NOT NULL,
+                    registered TIMESTAMP DEFAULT NOW()
+                    )
+                """,
+                """
+                CREATE TABLE IF NOT EXISTS redflags(
+                    redflag_id SERIAL PRIMARY KEY,
+                    created_by INT REFERENCES users(userid),
+                    incident_type VARCHAR (30) NOT NULL,
+                    location INT, 
+                    status VARCHAR(15),
+                    comment VARCHAR (225)
+                    )
+                """,
+                """
+                CREATE TABLE IF NOT EXISTS interventions(
+                    intervention_id SERIAL PRIMARY KEY,
+                    created_by INT REFERENCES users(userid),
+                    incident_type VARCHAR (30) NOT NULL,
+                    location INT, 
+                    status VARCHAR(15),
+                    comment VARCHAR (225)
+                    )
+                """,
+                
+                """
+                INSERT INTO users(username, password, isadmin)      
+                VALUES('admin','sup3rpsW' ,True)
+                """,
+                """
+                INSERT INTO users(username, password, isadmin)
+                VALUES('nonadmin','in1tial', False)
+                
+                """
+)
