57e55f0902c279e22ee7d888771ec7ab91c0be79
Rhytah <ritanamono@gmail.com>
2019-01-24 16:28:42 +0300

    declare heroku credentials

--- database/server.py
+++ database/server.py
@@ -21,21 +21,15 @@ class DatabaseConnect:
         self.cursor = self.conn.cursor()
         
 
-        if app.config.get('ENV') == 'development':
-            dbname = app_configuration['development'].DATABASE
-            self.credentials['dbname'] = dbname
-
-            
-        if app.config.get('ENV') == 'testing':
-            dbname = app_configuration['testing'].DATABASE
-            self.credentials['dbname'] = dbname
-
-        try:
+        if app.config.get('ENV') == 'production':
+            # dbname = app_configuration['production'].DATABASE_URI
+            self.credentials['host'] = app_configuration['production'].HOST
+            self.credentials['user'] = app_configuration['production'].USER
+            self.credentials['password'] = app_configuration['production'].PASSWORD
+            self.credentials['dbname'] = app_configuration['production'].DATABASE
             self.conn =  psycopg2.connect(**self.credentials, cursor_factory=RealDictCursor)
             self.conn.autocommit = True
             self.cursor = self.conn.cursor()
-        except:
-
             print("error:Failed to connect ")
 
     def drop_table(self,tablename):
