72eb24364d02ebb1d59396873c7f5d0a9d47b16d
Rhytah <ritanamono@gmail.com>
2019-01-25 05:30:22 +0300

    revert changes to server connection

--- config.py
+++ config.py
@@ -6,34 +6,32 @@ class Config:
     CSRF_ENABLED = True
     JWT_ACCESS_TOKEN_EXPIRES = False
     JWT_TOKEN_LOCATION = ['headers']
+    SECRET_KEY = b'\xc9$,\xa9\xdd;\xfeVz\xa0\x8f\xd5A,\x11\xf8\xf4\xceh\x94\xa9\x13\x80['
+    SECRET_KEY = os.getenv('SECRET_KEY', 'somewea')
     JWT_HEADER_NAME = 'Authorization'
     JWT_HEADER_TYPE = 'Bearer'
     DATABASE_URI = os.getenv('DATABASE_URL')
-    
+    PORT = 5432
+
 
 class DevelopmentConfig(Config):
     DEBUG=True
     ENV = 'development'
-    DATABASE = 'report_db'
+    DATABASE_URI = 'postgres://postgres:psql@localhost:5432/report_db'
     TESTING = False
 
 
 class TestingConfig(Config):
     DEBUG=True
     ENV = 'testing'
-    DATABASE = 'reporttest_db'
+    DATABASE_URI = 'postgres://postgres:psql@localhost:5432/reporttest_db'
     TESTING = True
 
 class ProductionConfig(Config):
     DEBUG=False
     ENV = 'production'
     DATABASE_URI= 'postgres://wkmnrsrpffhfpr:dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125@ec2-54-227-246-152.compute-1.amazonaws.com:5432/degbph26bv6m4i'
-    DATABASE = "degbph26bv6m4i" 
-    HOST = "ec2-54-227-246-152.compute-1.amazonaws.com" 
-    PASSWORD ="dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125"
-    USER = "wkmnrsrpffhfpr"
-    PORT = 5432
-    TESTING= False
+
 
 app_configuration = {
     "development":DevelopmentConfig,
--- database/server.py
+++ database/server.py
@@ -9,61 +9,43 @@ class DatabaseConnect:
 
     def __init__(self):
         
-        # self.credentials = dict(
-        #         dbname ='',
-        #         user = 'postgres',
-        #         password='mine',
-        #         host='localhost',
-        #         port = 5432
-        #     )
-        self.credentials= dict(dbname='degbph26bv6m4i',
-         user= 'wkmnrsrpffhfpr',
-          host='ec2-54-227-246-152.compute-1.amazonaws.com',
-         port =5432 ,
-         password = 'dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125' )
+        self.credentials = dict(
+                dbname ='',
+                user = 'postgres',
+                password='mine',
+                host='localhost',
+                port = 5432
+            )
         self.conn =  psycopg2.connect(**self.credentials, cursor_factory=RealDictCursor)
         self.conn.autocommit = True
         self.cursor = self.conn.cursor()
         
 
         if app.config.get('ENV') == 'development':
-            dbname = app_configuration['development'].DATABASE
+            dbname = app_configuration['development'].DATABASE_URI
             self.credentials['dbname'] = dbname
 
             
         if app.config.get('ENV') == 'testing':
-            dbname = app_configuration['testing'].DATABASE
+            dbname = app_configuration['testing'].DATABASE_URI
             self.credentials['dbname'] = dbname
     
-        # if app.config.get('ENV') == 'production':
+        if app.config.get('ENV') == 'production':
  
-        #     # self.credentials_heroku ="""
-        #     # dbname='degbph26bv6m4i' user= 'wkmnrsrpffhfpr' host='ec2-54-227-246-152.compute-1.amazonaws.com' port =5432 password = 'dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125' 
-        #     # # """
-        #     # self.credentials_heroku= dict(dbname='degbph26bv6m4i',
-        #     #  user= 'wkmnrsrpffhfpr',
-        #     #   host='ec2-54-227-246-152.compute-1.amazonaws.com',
-        #     #  port =5432 ,
-        #     #  password = 'dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125' )
-        #     # self.credentials =self.credentials_heroku
-        #     herokudb= app_configuration['production'].DATABASE_URI
-        #     self.credentials['user'] = app_configuration['production'].USER
-        #     self.credentials['password'] = app_configuration['production'].PASSWORD
-        #     self.credentials['host'] = app_configuration['production'].HOST
-        #     self.credentials['port'] = app_configuration['production'].PORT
-        #     self.credentials['dbname'] = dbname
-        #     # self.credentials = herokudb
-            
+            self.credentials_heroku ="""
+            dbname='degbph26bv6m4i' user= 'wkmnrsrpffhfpr' host='ec2-54-227-246-152.compute-1.amazonaws.com' port =5432 password = 'dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125' 
+            """
+            self.credentials =self.credentials_heroku
+        
         try:
             self.conn =  psycopg2.connect(**self.credentials, cursor_factory=RealDictCursor)
             self.conn.autocommit = True
             self.cursor = self.conn.cursor()
-            print(self.credentials['dbname'])
         except:
             print("failed to connect")
-        
     def drop_table(self,tablename):
         command = f"""
         DROP TABLE IF EXISTS {tablename} CASCADE
         """
         return self.cursor.execute(command)
+
