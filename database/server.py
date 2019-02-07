a4c64d07af279ff9e5b6a9555e11d204d250594a
Rhytah <ritanamono@gmail.com>
2019-02-07 11:03:42 +0300

    fix failing build

--- database/server.py
+++ database/server.py
@@ -1,5 +1,5 @@
 import psycopg2 
-from flask import current_app as app,abort
+from flask import current_app as app
 from config import app_configuration
 from psycopg2.extras import RealDictCursor
 from .relations_commands import sqlcommands
@@ -16,19 +16,36 @@ class DatabaseConnect:
                 host='localhost',
                 port = 5432
             )
-        self.credentials['dbname']=app_configuration['production'].DB
-        self.credentials['user']=app_configuration['production'].USER
-        self.credentials['password']=app_configuration['production'].PASSWORD
-        self.credentials['host']=app_configuration['production'].HOST
-        self.credentials['port']=app_configuration['production'].PORT
-        
         self.conn =  psycopg2.connect(**self.credentials, cursor_factory=RealDictCursor)
         self.conn.autocommit = True
         self.cursor = self.conn.cursor()
+        
 
+        if app.config.get('ENV') == 'development':
+            dbname = app_configuration['development'].DATABASE_URI
+            self.credentials['dbname'] = dbname
+
+            
+        if app.config.get('ENV') == 'testing':
+            dbname = app_configuration['testing'].DATABASE_URI
+            self.credentials['dbname'] = dbname
+    
+        if app.config.get('ENV') == 'production':
+ 
+            self.credentials_heroku ="""
+            dbname='degbph26bv6m4i' user= 'wkmnrsrpffhfpr' host='ec2-54-227-246-152.compute-1.amazonaws.com' port =5432 password = 'dde675f7f5af4dc53de4bbac1c7109921fa99454935ce281b3e94798c98eb125' 
+            """
+            self.credentials =self.credentials_heroku
+        
+        try:
+            self.conn =  psycopg2.connect(**self.credentials, cursor_factory=RealDictCursor)
+            self.conn.autocommit = True
+            self.cursor = self.conn.cursor()
+            print(self.credentials)
+        except:
+            print("failed to connect")
     def drop_table(self,tablename):
         command = f"""
         DROP TABLE IF EXISTS {tablename} CASCADE
         """
         return self.cursor.execute(command)
-
